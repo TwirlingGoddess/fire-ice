55567b3c42db455f9cddbe10073a65b9f70bd128
Lee Graham <graham.la3@gmail.com>
2018-09-13 15:29:12 -0600

    Implement actions tests

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,11 +2,25 @@ import * from '../actions';
 
 describe('actions', () => {
   
-  it('should add houses....' => {
-
+  it('should have a type of ADD_HOUSES' => {
+    const mockHouse = {name: "The Cow Jumped Over the Moon", founded: "Before the 3rd of Yore", id: 3}
+    const expectedAction = {
+      type: 'ADD_HOUSES',
+      mockHouse
+    }
+    const result = actions.addHouses(mockHouse)
+    expect(result).toEqual(expectedAction)
   })
 
-  it('should add members....' => {
-    
+
+  it('should have a type of ADD_MEMBERS' => {
+    const mockMembers = {name: "The Rock", died: "The othe day" id: 27 }
+    const expectedAction = {
+      type: 'ADD_MEMBERS',
+      mockMembers
+    }
+    const result = actions.addMembers(mockMembers)
+    expect(result).toEqual(expectedAction)
   })
+
 })
