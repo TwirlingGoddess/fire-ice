9c85c5df6d80959b2ff2be52de4fb8e75163d840
Lee Graham <graham.la3@gmail.com>
2018-09-13 15:51:45 -0600

    Implement app redux tests

---
+++ src/containers/App/App.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {  mapStateToProps, mapDispatchToProps } from './App';
+import { addHouses } from '../../actions';
+
+   describe('mapStateToProps', () => {
+    it('should return an object with the houses array', () => {
+      const mockState = {
+        name: "The Cow Jumped Over the Moon", founded: "Before the 3rd of Yore", id: 3
+      }
+      
+      const expected = {
+        houses: [{
+          name: "The Cow Jumped Over the Moon", founded: "Before the 3rd of Yore", id: 3
+        }]
+      }
+      const mappedProps = mapStateToProps(mockState)
+      expect(mappedProps).toEqual(expected)
+    })
+  })
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with a addHouses Action when addHouses is called', () => {
+      const mockHouse = {name: "The Cow Jumped Over the Moon", founded: "Before the 3rd of Yore", id: 3}
+      const mockDispatch = jest.fn();
+      const actionToDispatch = addHouses(mockHouse);
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.addHouses(mockHouse);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
